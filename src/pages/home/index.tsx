a3f338cc2bd37ff065ab03ec65ef99a46d053ad3
Juan Martin Nazarian <jmnazarian@gmail.com>
2022-10-18 19:09:12 -0300

    Select box completed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,24 @@
-import { Typography } from "@mui/material"
+import { Button, Grid, Typography } from "@mui/material"
 import { Stack } from "@mui/system"
 import React, { useState } from "react"
 import { SelectBar } from "../../components/selectBar"
 import CircularProgress from '@mui/material/CircularProgress';
-
+import { NutrientBox } from "../../components/nutrientBox";
 import axios from 'axios';
 
 export const HomePage: React.FC<{}> = () => {
   
-  const [selectedNutrients, setSelectedNutrients] = useState<any[]>([])
+  const [selectedNutrients, setSelectedNutrients] = useState<any[]>([]);
+
+  const clickHandle = (nutrientId: string) => {
+    const remainingNutrients = selectedNutrients.filter( item => item.nutrientId !== nutrientId);
+    setSelectedNutrients(remainingNutrients);
+  }
+
 
   const getNutrientsList = async () => {
     // 'https://timely-flan-5fe4e8.netlify.app/.netlify/functions/getNutrientsList'
-    const res = await axios('http://localhost:8888/.netlify/functions/getNutrientsList', {
+    const res = await axios.get('http://localhost:8888/.netlify/functions/getNutrientsList', {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
@@ -22,10 +28,20 @@ export const HomePage: React.FC<{}> = () => {
     return res?.data as nutrientItem[];
   }
 
-  console.log('selectedNutrients');
-  console.log(selectedNutrients);
-  console.log('selectedNutrients');
+  const fetchFood = async () => {
+    const nutrients:any[] = [];
+    selectedNutrients.map( sn => nutrients.push({nutrientId: sn.nutrientId, name:sn.name}));
 
+    // 'https://timely-flan-5fe4e8.netlify.app/.netlify/functions/getFoodByNutrients'
+    const res = await axios.post('http://localhost:8888/.netlify/functions/getFoodByNutrients',
+    JSON.stringify({ "nutrients": nutrients}));
+
+    console.log(res?.data)
+
+    return res?.data as Food[];
+  }
+
+  console.log(selectedNutrients);
 
   return (
     <Stack direction="column" width='100%' mt='2em'>
@@ -41,8 +57,30 @@ export const HomePage: React.FC<{}> = () => {
         loadingComponent = {<CircularProgress />}
         setSelectedItems = {setSelectedNutrients}
       />
-
-      
+      {
+        selectedNutrients.length > 0 && (
+          <Stack width='80vw' margin='2em auto'>
+            <Grid container margin='2em 2em' direction='row'>
+              {
+                selectedNutrients.map( item => (
+                  <Grid item key={item.nutrientId}>
+                    <NutrientBox
+                    nutrient = {item}
+                    onDelete={() => clickHandle(item.nutrientId)}
+                    />
+                  </Grid>
+                  ))
+              }
+            </Grid>
+            <Button
+              variant="contained"
+              onClick={fetchFood}
+            >
+              Search
+            </Button>
+          </Stack>
+        )
+      }
       
     </Stack>
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -5,8 +5,22 @@ type SelectBarProps = {
   setSelectedItems: Function;
 }
 
+type NutrientBox = {
+  nutrient: nutrientItem;
+  onDelete: Function;
+}
+
 type nutrientItem = {
   nutrientId: string;
   name: string;
   rank?: string;
+}
+
+type Food = {
+  fdcId: number
+  ndbNumber: number
+  foodCategory: string
+  dataType: string
+  description: string
+  publicationDate: string
 }
